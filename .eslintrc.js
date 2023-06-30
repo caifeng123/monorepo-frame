7e29365e57c5910528f9659daaf77b131cd0d506
caifeng123 <594745425@qq.com>
2023-06-30 10:57:10 +0800

    feat: prettier与eslint统一, node tsc编译问题修复

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
   extends: ['airbnb-typescript/base'],
   plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    '@typescript-eslint/comma-dangle': ['error', 'never']
+  }
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,6 @@
 module.exports = {
-  semi: false, // 不使用分号
   singleQuote: true, // 使用单引号
   printWidth: 80, // 每行代码的最大宽度
   trailingComma: 'none', // 不使用尾逗号
   arrowParens: 'avoid' // 对于单参数箭头函数，省略参数括号
-}
+};
