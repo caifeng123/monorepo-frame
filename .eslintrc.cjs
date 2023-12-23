04a05fbb87437c3cef6b9dac1c999b6347933b85
caifeng.nice <caifeng.nice@bytedance.com>
2023-12-23 20:34:22 +0800

    feat: extract eslint config & use turbo to cache output

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,25 +4,36 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json'
   },
-  ignorePatterns: ['**/output/*', '**/es/*', '**/lib/*', '**/cjs/*'],
+  ignorePatterns: ['**/output/*', '**/es/*', '**/lib/*', '**/cjs/*', '**/dist/*'],
   settings: {
+    // 设置内部库前缀 避免eslint无法找到
     'import/internal-regex': /^@cc\//,
     'import/resolver': {
       typescript: {}
     }
   },
   extends: [
-    // 有react相关
+    // airbnb ts配置
+    // https://www.npmjs.com/package/eslint-config-airbnb-typescript
     'airbnb',
-    'airbnb-typescript'
+    'airbnb-typescript',
 
-    // 纯node库可用 https://www.npmjs.com/package/eslint-config-airbnb-base
-    // airbnb-base
+    // 带react
+    'plugin:react/jsx-runtime'
   ],
-  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'simple-import-sort'],
+  plugins: [
+    // ts插件
+    '@typescript-eslint/eslint-plugin',
+    // 导入文件
+    'import',
+    // 文件导入导出排序
+    'simple-import-sort'
+  ],
   rules: {
+    // 禁用尾逗号
     'comma-dangle': ['error', 'never'],
     '@typescript-eslint/comma-dangle': ['error', 'never'],
+    // 文件导入导出排序
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error'
   }
--- packages/react/.eslintrc.cjs
+++ packages/react/.eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json'
   },
-  extends: ['plugin:react/jsx-runtime'],
   rules: {
     'import/no-extraneous-dependencies': [
       'error',
