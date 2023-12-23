1ce11fd7fdfc259f7972d77c0c31a423d8d3438b
caifeng.nice <caifeng.nice@bytedance.com>
2023-12-23 20:40:07 +0800

    fix: remove extra eslint config

--- packages/react/.eslintrc.cjs
+++ packages/react/.eslintrc.cjs
@@ -1,13 +1,5 @@
 module.exports = {
   parserOptions: {
     project: './tsconfig.json'
-  },
-  rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: ['**/rsbuild.config.ts']
-      }
-    ]
   }
 };
